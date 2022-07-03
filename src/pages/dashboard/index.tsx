a1e038fae361efcd8d43e67b948798a37211782d
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2022-07-03 16:07:29 +0100

    small changes on the dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -11,17 +11,32 @@ const Dashboard: NextPage = () => {
   const { data } = useSession();
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello, {JSON.stringify(data)}!
-      </h1>
-      <button
-        className="btn btn-primary"
-        onClick={() => signOut({ callbackUrl: "/" })}
-      >
-        Logout
-      </button>
-    </>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content">
+        <div className="max-w-lg">
+          <h1 className="text-5xl text-center font-bold leading-snug text-gray-400">
+            You are logged in!
+          </h1>
+          <p className="my-4 text-center leading-loose">
+            You are allowed to visit this page because you have a session,
+            otherwise you would be redirected to the login page.
+          </p>
+          <div className="my-4 bg-gray-700 rounded-lg p-4">
+            <pre>
+              <code>{JSON.stringify(data, null, 2)}</code>
+            </pre>
+          </div>
+          <div className="text-center">
+            <button
+              className="btn btn-secondary"
+              onClick={() => signOut({ callbackUrl: "/" })}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
